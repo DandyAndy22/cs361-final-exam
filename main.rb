7cb487b455c55c36fcd46fc8a95b25c8bd2f705f
Andras Mihaly <andrasm96@gmail.com>
2020-03-18 11:44:19 -0700

    Refactor Final Main: Added object instantiations to main file
    References #1

--- main.rb
+++ main.rb
@@ -7,12 +7,21 @@ require_relative 'bmx_bike'
 require_relative 'road_bike'
 require_relative 'mountain_bike'
 require_relative 'price_calculator'
+require_relative 'tail_pack'
+require_relative 'pannier'
+require_relative 'roll_pack'
+
+tail_pack = TailPack.new()
+pannier_1 = Pannier.new()
+pannier_2 = Pannier.new()
+panniers = [pannier_1, pannier_2]
+roll_pack = RollPack.new()
 
 bikes = [
-  BmxBike.new,
-  RoadBike.new,
-  MountainBike.new,
-  MountainBike.new
+  BmxBike.new(tail_pack),
+  RoadBike.new(panniers),
+  MountainBike.new(roll_pack),
+  MountainBike.new(roll_pack)
 ]
 
 total = PriceCalculator.new(bikes)
