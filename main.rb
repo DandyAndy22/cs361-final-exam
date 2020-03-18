1c84c08272271f83b31135538040f445afdbc134
Andras Mihaly <andrasm96@gmail.com>
2020-03-18 11:30:00 -0700

    Refactor Final Main: Extracted price calculation from main
    References  #1

--- main.rb
+++ main.rb
@@ -6,6 +6,7 @@
 require_relative 'bmx_bike'
 require_relative 'road_bike'
 require_relative 'mountain_bike'
+require_relative 'price_calculator'
 
 bikes = [
   BmxBike.new,
@@ -14,15 +15,6 @@ bikes = [
   MountainBike.new
 ]
 
-total_price = 0
-total_price += bikes[0].weekly_rate
-total_price += bikes[0].tail_pack.cost
-total_price += bikes[1].daily_rate * 7
-total_price += bikes[1].panniers[0].price
-total_price += bikes[1].panniers[1].price
-total_price += bikes[2].weekly_rate
-total_price += bikes[2].luggage.price
-total_price += bikes[3].weekly_rate
-total_price += bikes[3].luggage.price
+total = PriceCalculator.new(bikes)
 
-puts "Total price: #{total_price}"
+puts "Total price: #{total.price}"
